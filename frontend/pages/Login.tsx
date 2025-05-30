93f40b68e4cffe54a439943c06d625c9b3b28f89
suryaprabhat <maddalasuryaprabhat@gmail.com>
2025-05-30 20:04:38 +0530

    first commit

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "../Styles/Login.css";
+import "../Styles/login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
